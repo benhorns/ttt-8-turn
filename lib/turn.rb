8ac59e31a7110e3d56d2c85ba88e8cd9d8dd8d2a
Benjamin Horns <benhorns@gmail.com>
2019-12-11 22:25:25 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,14 +5,25 @@ def display_board (board)
   puts "-----------"
   puts "   |   |   "
 end
-input = user_input
-until input == 5
+
+def valid_move?(board,index)
+
+index.between?(0, 8) && !position_taken?(board,index)
+
+end
+
+def position_taken? (board,index)
+if board[index] == " "
+
+else board[index] != " "
+
+end
+end
 
   def input_to_index(user_input)
     user_input.to_i - 1
   end
   def turn(board)
     puts "Please enter 1-9:"
-
   end
 end
