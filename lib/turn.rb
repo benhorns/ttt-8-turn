66d42b9708436c8d1c4e898f0ac545a96bf31620
Benjamin Horns <benhorns@gmail.com>
2019-12-11 22:20:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ def display_board (board)
   puts "   |   |   "
 end
 
-  until user_unput.between(1,9)
+  until user_input.between(1,9)
   def input_to_index(user_input)
     user_input.to_i - 1
   end
