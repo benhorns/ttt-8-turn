5bdce0718bde06ba392afaa0908d03373f75bb31
Benjamin Horns <benhorns@gmail.com>
2019-12-11 22:21:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,13 +6,12 @@ def display_board (board)
   puts "   |   |   "
 end
 
-  until input.between(1,9)
   def input_to_index(user_input)
     user_input.to_i - 1
   end
-
+  until input = 5
   def turn(board)
     puts "Please enter 1-9:"
-
+end
   end
 end
