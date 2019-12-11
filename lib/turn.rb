9f70e200f1666dd021950cd0089f7aa08d8c1c3a
Benjamin Horns <benhorns@gmail.com>
2019-12-11 22:12:06 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+def display_board (board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def turn(board)
+
+  puts "Please enter 1-9:"
+
+end
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
